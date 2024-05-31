e48c9bc22892229a36f1155ced1a99f025b670d4
Alltxe <148350310+Alltxe@users.noreply.github.com>
2024-05-31 09:59:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QVBoxLayout, QWidget
 from challenge import Challenge
 from training import Training
 from templates.main_menu import Ui_Form
@@ -7,7 +7,7 @@ from result import Result
 from rules import Rules
 
 
-class MenuWidget(Ui_Form, QMainWindow):
+class MenuWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -17,15 +17,25 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.stacked_widget = QStackedWidget()
-        self.setCentralWidget(self.stacked_widget)
         self.setWindowTitle('Тренажер для памяти')
+        self.resize(800, 600)  # Установка начального размера окна
 
-        self.setFixedSize(800, 600)
+        # Создание главного виджета и установка его как центрального
+        main_widget = QWidget(self)
+        self.setCentralWidget(main_widget)
 
+        # Создание вертикального макета
+        self.layout = QVBoxLayout(main_widget)
+
+        # Создание QStackedWidget и добавление его в основной макет
+        self.stacked_widget = QStackedWidget()
+        self.layout.addWidget(self.stacked_widget)
+
+        # Инициализация основного меню
         self.main_menu = MenuWidget()
         self.stacked_widget.addWidget(self.main_menu)
 
+        # Подключение кнопок основного меню к соответствующим методам
         self.main_menu.training_btn.clicked.connect(lambda: self.switch_widget('training'))
         self.main_menu.challenge_btn.clicked.connect(lambda: self.switch_widget('challenge'))
         self.main_menu.info_btn.clicked.connect(lambda: self.switch_widget('rules'))
